a289a6b82c861483eeeabe6bfbd8e5ca8c6978cc
mankindmember12 <130433831+mankindmember12@users.noreply.github.com>
2023-07-09 00:30:25 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import glob from 'glob';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: 'resources/js/app.jsx',
-      ssr: 'resources/js/ssr.jsx',
-      refresh: true,
-    }),
-    react(),
-  ],
-  root: path.join(__dirname, "src"),
-  build: {
-    outDir: path.join(__dirname, "dist"),
-    rollupOptions: {
-      input: glob.sync(path.resolve(__dirname, "src", "*.jsx")),
-    },
-  },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            ssr: 'resources/js/ssr.jsx',
+            refresh: true,
+        }),
+
+        react(),
+    ],
 });
